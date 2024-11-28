3a797f34ee0e8bd4dad2a2ff2afa839c4c07368b
UnaHermosa <veintitress@yandex.ru>
2024-11-28 13:50:05 +0300

    Правки после ревью: улучшение кода

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-function createCard (link, name, del) {
-  const cardTemplate = document.querySelector('#card-template').content;
+function createCard (link, name, onDelete) {
   const card = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImage = card.querySelector('.card__image');
   const cardTitle = card.querySelector('.card__title');
@@ -11,13 +11,13 @@ function createCard (link, name, del) {
   cardImage.alt = name;
   cardTitle.textContent = name;
 
-  deleteButton.addEventListener('click', del);
+  deleteButton.addEventListener('click', () => deleteCard(card));
   
   return card;
 }
 
-function deleteCard (evt) {
-  evt.target.parentElement.remove();
+function deleteCard (card) {
+  card.remove();
 }
 
 initialCards.forEach(item => placesList.append(createCard(item.link, item.name, deleteCard)));
