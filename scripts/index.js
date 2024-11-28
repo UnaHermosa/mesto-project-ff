6ec0143e6f0891da5bd10e603c4987452dffd6a0
UnaHermosa <veintitress@yandex.ru>
2024-11-28 09:24:14 +0300

    Создана функция создания карточки, карточки отрисованы на странице

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
+const placesList = document.querySelector('.places__list');
+
+function createCard (link, name) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+
+  placesList.append(card);
+}
+
+initialCards.forEach(item => createCard(item.link, item.name));
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
