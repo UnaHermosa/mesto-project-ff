0085e0ff8c2c3186808fa24360b18319006e27ac
UnaHermosa <veintitress@yandex.ru>
2024-11-28 10:54:57 +0300

    Реализовано удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,27 @@
 const placesList = document.querySelector('.places__list');
 
-function createCard (link, name) {
+function createCard (link, name, del) {
   const cardTemplate = document.querySelector('#card-template').content;
   const card = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImage = card.querySelector('.card__image');
   const cardTitle = card.querySelector('.card__title');
+  const deleteButton = card.querySelector('.card__delete-button');
 
   cardImage.src = link;
   cardImage.alt = name;
   cardTitle.textContent = name;
 
-  placesList.append(card);
+  deleteButton.addEventListener('click', del);
+  
+  return card;
 }
 
-initialCards.forEach(item => createCard(item.link, item.name));
+function deleteCard (evt) {
+  evt.target.parentElement.remove();
+}
+
+initialCards.forEach(item => placesList.append(createCard(item.link, item.name, deleteCard)));
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
