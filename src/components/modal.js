b32cd6908a0abdcba54f75f4b21692eda0052e99
UnaHermosa <veintitress@yandex.ru>
2025-01-03 12:15:39 +0300

    Реализованы функции открытия/закрытия модальных окон редактирования профиля, добавления новой карточки, функция добавления новой карточки

--- src/components/modal.js
+++ src/components/modal.js
@@ -2,12 +2,33 @@
 
 function openModal(popup) {
   popup.classList.add('popup_is-opened');
+  document.addEventListener('keydown', handleCloseByEsc);
+  popup.addEventListener('click', handlerCloseByOverlay);
 }
 
 // Закрытие модального окна
 
 function closeModal(popup) {
   popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', handleCloseByEsc);
+  popup.removeEventListener('click', handlerCloseByOverlay);
+}
+
+// Закрытие модального окна при нажатии Esc
+
+function handleCloseByEsc(evt) {
+  if(evt.key === 'Escape') {
+    const popupIsOpened = document.querySelector('.popup_is-opened');
+    closeModal(popupIsOpened);
+  }
+}
+
+// Закрытие модального окна при клике на оверлей
+
+function handlerCloseByOverlay(evt) {
+  if(evt.target.classList.contains('popup')) {
+    closeModal(evt.target);
+  }
 }
 
 export { openModal, closeModal };
--- src/index.js
+++ src/index.js
@@ -4,37 +4,83 @@ import {initialCards} from '../src/scripts/cards';
 import { createCard, deleteCard } from '../src/components/card';
 import { closeModal, openModal } from '../src/components/modal';
 
-import './vendor/fonts/Inter-Black.woff2';
-import './vendor/fonts/Inter-Medium.woff2';
-import './vendor/fonts/Inter-Regular.woff2';
+import '../src/vendor/fonts/Inter-Black.woff2';
+import '../src/vendor/fonts/Inter-Medium.woff2';
+import '../src/vendor/fonts/Inter-Regular.woff2';
 
-import './images/add-icon.svg';
+import '../src/images/add-icon.svg';
 import '../src/images/avatar.jpg';
-import './images/card_1.jpg';
-import './images/card_2.jpg';
-import './images/card_3.jpg';
-import './images/close.svg';
-import './images/delete-icon.svg';
-import './images/edit-icon.svg';
-import './images/like-active.svg';
-import './images/like-inactive.svg';
-import './images/logo.svg';
-
-// Отрисовка карточек на странице
+import '../src/images/card_1.jpg';
+import '../src/images/card_2.jpg';
+import '../src/images/card_3.jpg';
+import '../src/images/close.svg';
+import '../src/images/delete-icon.svg';
+import '../src/images/edit-icon.svg';
+import '../src/images/like-active.svg';
+import '../src/images/like-inactive.svg';
+import '../src/images/logo.svg';
 
 const placesList = document.querySelector('.places__list');
 
+const profileTitle = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__description');
+
+// Отрисовка карточек на странице
+
 initialCards.forEach(item => placesList.append(createCard(item.link, item.name, deleteCard)));
 
-// Открытие и закрытие попапа редактирования профиля
+// Открытие и закрытие модального окна редактирования профиля
 
-const popup = document.querySelector('.popup');
-const editProfileButton = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup_type_edit');
-const popupClose = popup.querySelector('.popup__close');
+const editProfileButton = document.querySelector('.profile__edit-button');
+const closeButtonEdit = popupEdit.querySelector('.popup__close');
 
-editProfileButton.addEventListener('click', () => openModal(popupEdit));
-popupClose.addEventListener('click', () => closeModal(popupEdit));
+editProfileButton.addEventListener('click', () => {
+  profileName.value = profileTitle.textContent;
+  profileInputDescription.value = profileJob.textContent;
+  openModal(popupEdit);
+});
+closeButtonEdit.addEventListener('click', () => closeModal(popupEdit));
 
-// Открытие и закрытие попапа изображения карточки
+// Обработчик формы редактирования профиля
+
+const formEdit = popupEdit.querySelector('.popup__form');
+const profileName = formEdit.querySelector('.popup__input_type_name');
+const profileInputDescription = formEdit.querySelector('.popup__input_type_description');
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.value = profileTitle.textContent;
+  profileInputDescription.value = profileJob.textContent;
+}
+
+formEdit.addEventListener('submit', handleFormSubmit);
+
+// Открытие и закрытие модального окна добавления карточки
+
+const popupNewCard = document.querySelector('.popup_type_new-card');
+const addButton = document.querySelector('.profile__add-button');
+const closeButtonNewCard = popupNewCard.querySelector('.popup__close');
+const urlNewCard = popupNewCard.querySelector('.popup__input_type_url');
+const placeNameNewCard = popupNewCard.querySelector('.popup__input_type_card-name');
+
+addButton.addEventListener('click', () => openModal(popupNewCard));
+closeButtonNewCard.addEventListener('click', () => closeModal(popupNewCard));
+
+// Добавление карточки
+const formNewCard = popupNewCard.querySelector('.popup__form');
+
+function addCard(evt) {
+  evt.preventDefault();
+  const cardData = {
+    url: urlNewCard.value,
+    place: placeNameNewCard.value
+  }
+  
+  placesList.prepend(createCard(cardData.url, cardData.place, deleteCard));
+}
+
+formNewCard.addEventListener('submit', addCard);
+
+// Открытие и закрытие модального окна изображения карточки
 
